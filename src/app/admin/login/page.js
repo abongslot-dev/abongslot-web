fecba438c46f86a80df9cecbf1aa7e28b838527b
abongslot-dev <rsh74772@gmail.com>
2026-03-06 09:19:50 +0700

    Update page.js

--- src/app/admin/login/page.js
+++ src/app/admin/login/page.js
@@ -4,7 +4,7 @@ import { createClient } from "@supabase/supabase-js";
 import { useRouter } from "next/navigation";
 import { Lock, Mail, ShieldCheck, Loader2 } from "lucide-react";
 
-import { supabase } from "@/lib/supabase";
+import { supabase } from "../../lib/supabase";
 
 // GUNAKAN INI, JANGAN PAKAI 'createClientComponentClient'
 const supabase = createClient(
