0a2e41716723c09f2f3302373f13a7714843224f
abongslot-dev <rsh74772@gmail.com>
2026-03-06 08:57:11 +0700

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,11 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 
+// Pakai variabel yang sudah kamu buat tadi
 const SUPABASE_URL = 'https://hqsahuywehlbwywyzlsz.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_PiwkCSc05QG4DjULYyUjTw_0R1uUux6'
 
-// Buat satu koneksi pusat yang bisa dipakai di mana saja
+// Export variabel 'supabase' agar bisa di-import oleh file lain
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-// Kita buat fungsi default agar kodingan lama tidak error
+// Fungsi pembantu tambahan
 const db = () => supabase;
 export default db;
