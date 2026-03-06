1c13666da782034bdb574e7e30a17f413a3a70b1
abongslot-dev <rsh74772@gmail.com>
2026-03-07 02:37:02 +0700

    Update page.js

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import Link from 'next/link';
 import { useRouter } from "next/navigation"; // INI YANG KURANG
-import { supabase } from "../../lib/supabase";
+import { supabase } from "./supabase";
 import { 
   Search, RotateCcw, CheckCircle2, XCircle, 
   ChevronDown, Landmark, ArrowRightLeft, LayoutDashboard, 
