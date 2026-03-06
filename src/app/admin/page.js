a59318af3c5dd4e633fc6e8ab564eabac034b773
abongslot-dev <rsh74772@gmail.com>
2026-03-06 08:40:09 +0700

    Update page.js

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState, useEffect } from "react";
 import Link from 'next/link';
+import { useRouter } from "next/navigation"; // INI YANG KURANG
 import { 
   Search, RotateCcw, CheckCircle2, XCircle, 
   ChevronDown, Landmark, ArrowRightLeft, LayoutDashboard, 
