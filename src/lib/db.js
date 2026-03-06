57686160a5d246a50ca3f017229a3a7839ae22a8
abongslot-dev <rsh74772@gmail.com>
2026-03-06 08:58:34 +0700

    Update db.js

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Pakai variabel yang sudah kamu buat tadi
 const SUPABASE_URL = 'https://hqsahuywehlbwywyzlsz.supabase.co'
 const SUPABASE_KEY = 'sb_publishable_PiwkCSc05QG4DjULYyUjTw_0R1uUux6'
 
-// Export variabel 'supabase' agar bisa di-import oleh file lain
+// Koneksi utama yang dipanggil oleh halaman admin
 export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY)
 
-// Fungsi pembantu tambahan
 const db = () => supabase;
 export default db;
