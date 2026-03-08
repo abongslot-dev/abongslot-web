45da369b347ad8bcf6a73e93ebbba2327120de78
abongslot-dev <rsh74772@gmail.com>
2026-03-09 03:56:40 +0700

    Update page.js

--- src/app/hubungi/page.js
+++ src/app/hubungi/page.js
@@ -19,19 +19,40 @@ export default function Hubungi() {
   const [username, setUsername] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  const daftarKontak = [
-    { name: "Live Chat", desc: "Layanan 24 Jam Nonstop", icon: "💬", color: "from-blue-600 to-blue-400", link: "#", action: "Chat Sekarang" },
-    { name: "WhatsApp", desc: "Respon Cepat & Ramah", icon: "📱", color: "from-green-600 to-green-400", link: "https://wa.me/628123456789", action: "Kirim Pesan" },
-    { name: "Telegram", desc: "Update Promo Tercepat", icon: "✈️", color: "from-sky-600 to-sky-400", link: "https://t.me/username", action: "Gabung Grup" },
-    { 
-  name: "Instagram", 
-  desc: "Follow Untuk Info Event", 
-  icon: <img src="https://photoku.io/images/2024/09/25/ig.png" className="w-11 h-11 object-contain" alt="IG" />, 
-  color: "from-purple-600 to-pink-500", 
-  link: "https://instagram.com/username", 
-  action: "Follow Kami" 
-}
-  ];
+const daftarKontak = [
+  { 
+    name: "Facebook", 
+    desc: "info-group Terbaru", 
+    icon: <img src="https://photoku.io/images/2024/09/25/fb.png" className="w-11 h-11 object-contain" alt="Facebook" />, 
+    color: "from-blue-600 to-blue-400", 
+    link: "#", 
+    action: "Cek Sekarang" 
+  },
+  { 
+    name: "WhatsApp", 
+    desc: "Respon Cepat & Ramah", 
+    icon: <img src="https://photoku.io/images/2024/09/25/wa.png" className="w-11 h-11 object-contain" alt="WA" />, 
+    color: "from-green-600 to-green-400", 
+    link: "https://wa.me/628123456789", 
+    action: "Kirim Pesan" 
+  },
+  { 
+    name: "Telegram", 
+    desc: "Update Promo Tercepat", 
+    icon: <img src="https://photoku.io/images/2024/09/25/tele.png" className="w-11 h-11 object-contain" alt="Tele" />, 
+    color: "from-sky-600 to-sky-400", 
+    link: "https://t.me/username", 
+    action: "Gabung Grup" 
+  },
+  { 
+    name: "Instagram", 
+    desc: "Follow Untuk Info Event", 
+    icon: <img src="https://photoku.io/images/2024/09/25/ig.png" className="w-11 h-11 object-contain" alt="IG" />, 
+    color: "from-purple-600 to-pink-500", 
+    link: "https://instagram.com/username", 
+    action: "Follow Kami" 
+  }
+];
 
   const menuLainnya = [
     { name: "RTP 99%", icon: "🎰", link: "#" },
