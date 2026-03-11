e83d75be8993be2bc26bcd37659a136125963113
abongslot-dev <rsh74772@gmail.com>
2026-03-12 04:42:13 +0700

    Update layout.js

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -16,7 +16,7 @@ const handleLogout = () => {
     // Hapus cookie dengan cara set expire ke masa lalu
     document.cookie = "auth_token=; path=/; expires=Thu, 01 Jan 1970 00:00:01 GMT;";
     
-    router.push("/admin/login");
+    router.push("/login");
     router.refresh(); 
   }
 };
